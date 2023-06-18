eb81952f0a2a88267803f39a79df82a734b33d63
Edilson Costa <91851015+Edilsoncostajunior@users.noreply.github.com>
2023-06-17 22:28:43 -0300

    clientControllerTest

--- src/test/java/com/grupo4/controllers/CartControllerTest.java
+++ src/test/java/com/grupo4/controllers/CartControllerTest.java
@@ -1,35 +1,90 @@
 package com.grupo4.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.grupo4.models.CartProduct;
+import com.grupo4.models.Product;
+
 public class CartControllerTest {
-    @Test
-    void testMakeChanges() {
+    private CartController cartController;
+    private String clientId;
+    private String storeId;
+    private Product product;
 
+    @BeforeEach
+    public void setUp() {
+        ProductController store_ProductController = ProductController.getInstance("1");
+        clientId = "1";
+        storeId = "1";
+        cartController = CartController.getInstance(clientId, storeId);
+        product = store_ProductController.product_GET_BY_ID("10");
     }
 
     @Test
-    void testPutInTheCart() {
+    public void testPutInTheCart() {
+        int initialSize = cartController.product_GET_ALL().size();
 
+        cartController.PutInTheCart(product, 1);
+
+        List<CartProduct> products = cartController.product_GET_ALL();
+        Assertions.assertEquals(initialSize + 1, products.size());
+
+        CartProduct cartProduct = products.get(products.size() - 1);
+        Assertions.assertEquals(storeId, cartProduct.getStore_id());
+        Assertions.assertEquals(product.getId(), cartProduct.getProduct_id());
+        Assertions.assertEquals(product.getName(), cartProduct.getProduct_id());
+        Assertions.assertEquals(2, cartProduct.getQuantity());
     }
 
     @Test
-    void testTakeOfFromTheCart() {
+    public void testMakeChanges() {
+        cartController.PutInTheCart(product, 2);
+        List<CartProduct> products = cartController.product_GET_ALL();
+        CartProduct cartProduct = products.get(0);
+        String cartProductId = cartProduct.getId();
 
+        Map<String, String> changes = new HashMap<>();
+        changes.put("id", cartProductId);
+        changes.put("quantity", "3");
+        changes.put("productName", "Updated Product");
+
+        cartController.MakeChanges(changes);
+
+        CartProduct updatedCartProduct = cartController.product_GET_BY_ID(cartProductId);
+        Assertions.assertEquals(3, updatedCartProduct.getQuantity());
+        Assertions.assertEquals("Updated Product", updatedCartProduct.getProduct_id());
     }
 
     @Test
-    void testBuyProducts() {
+    public void testTakeOfFromTheCart() {
+        cartController.PutInTheCart(product, 2);
+        List<CartProduct> products = cartController.product_GET_ALL();
+        int initialSize = products.size();
+        CartProduct cartProduct = products.get(0);
+        String cartProductId = cartProduct.getId();
 
+        cartController.TakeOfFromTheCart(cartProductId);
+
+        List<CartProduct> updatedProducts = cartController.product_GET_ALL();
+        Assertions.assertEquals(initialSize - 1, updatedProducts.size());
+        Assertions.assertFalse(updatedProducts.stream().anyMatch(p -> p.getId().equals(cartProductId)));
     }
 
     @Test
-    void testProduct_GET_ALL() {
+    public void testBuyProducts() {
+        cartController.PutInTheCart(product, 2);
+        List<CartProduct> products = cartController.product_GET_ALL();
+        Assertions.assertFalse(products.isEmpty());
 
-    }
+        cartController.buyProducts();
 
-    @Test
-    void testProduct_GET_BY_ID() {
-
+        List<CartProduct> updatedProducts = cartController.product_GET_ALL();
+        Assertions.assertTrue(updatedProducts.isEmpty());
     }
 }
