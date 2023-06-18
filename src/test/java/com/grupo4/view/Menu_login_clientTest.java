6ec9122021183267d547c9c6e5005153312d99ef
Edilson Costa <91851015+Edilsoncostajunior@users.noreply.github.com>
2023-06-17 23:05:13 -0300

    Views
    criação das classes de alguns testes de views.

---
+++ src/test/java/com/grupo4/view/Menu_login_clientTest.java
@@ -0,0 +1,15 @@
+package com.grupo4.view;
+
+import org.junit.jupiter.api.Test;
+
+public class Menu_login_clientTest {
+    @Test
+    void testInit() {
+
+    }
+
+    @Test
+    void testRun() {
+
+    }
+}
